a841bcfc49eed0fcb46ac149f09266ba02c7184b
Rob Owens <rtowens17@gmail.com>
2019-02-02 09:02:46 -0600

    Updated comments
    Provided Functions with descriptions and inside more detailed explanations

--- Cribbage/cards.h
+++ Cribbage/cards.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once // make sure this header file is only compiled once
 #include <vector> // for the list of cards
 #include <cassert> // for assertions 
 
@@ -154,7 +154,7 @@ public: // ========== METHODS ==========
 	{
 		// starting at the front, check for a match
 		for (int i = 0; i < cards.size(); i++)
-			if (cards[i] == c) // if a match is found return the i + difition
+			if (cards[i] == c) // if a match is found return the position
 				return i;
 		return -1; // return -1 if no match
 	}
@@ -195,14 +195,14 @@ public: // ========== METHODS ==========
 	void operator += (card c) { add(c); }
 
 	// ===== '[]' =====
-	// returns the card in i + difition 'index' from cards
+	// returns the card in position 'index' from 'cards'
 	card operator [] (int index)
 	{
 		// if index is out of bounds return generic constructor
 		if (index >= size() || index < 0)
 			return card();
 
-		// return card in i + difition 'index'
+		// return card in position 'index'
 		return cards[index];
 	}
 
@@ -218,7 +218,7 @@ public: // ========== METHODS ==========
 	}
 
 	// ===== '-' =====
-	// will return the deck without the card c
+	// will return the deck without the card 'c'
 	deck operator - (card c)
 	{
 		deck new_deck(this);
@@ -227,7 +227,8 @@ public: // ========== METHODS ==========
 		return new_deck;
 	}
 
-
+	// ===== '+' =====
+	// adds the card 'c' to the deck
 	deck operator + (card c)
 	{
 		deck new_deck(this);
@@ -328,13 +329,16 @@ public:
 	}
 
 	// ===== PICK CRIB CARDS =====
-	// will move 2 cards from this->hand to dealer->crib
+	// will move 2 cards from 'this->hand' to 'dealer->crib'
 	// will remove the 2 moved cards from the hand
 	void pick_crib_cards(player * dealer, std::ostream &out_s)
 	{
-		int c1 = -1, c2 = -1;
+		int c1 = -1; // the first card to send to the crib
+		int c2 = -1; // the second card to send to the crib
+		// while valid cards are NOT picked (numbers 1 -> 6 and not the same)
 		while (c1 < 0 || c1 >= hand.size() || c2 < 0 || c2 >= hand.size() || c2 == c1)
 		{
+			// print the hand
 			hand.print(out_s); out_s << std::endl;
 			if (dealer == this)
 				std::cout << "Pick cards to send to your crib: ";
@@ -344,9 +348,11 @@ public:
 			--c1, --c2;
 		}
 
+		// add the cards to the crib
 		dealer->crib += hand[c1];
 		dealer->crib += hand[c2];
-
+ 		
+		// remove the cards from this player's hand
 		hand.remove(c1);
 		if (c2 > c1)
 			c2--;
@@ -373,14 +379,14 @@ public:
 		// if not, return an error card
 		if (!can_play) { return card(); }
 
-		int c = -1; // the card i + difition
+		int c = -1; // the card position
 		while (c < 0 || c >= hand.size())
 		{
 			// print the hand
 			hand.print(out_s); 
 			std::cout << "Pick a card to play: ";
 			std::cin >> c;
-			// decrease the i + difition since people start counting at 1
+			// decrease the position since people start counting at 1
 			--c; 
 			// if the selected card is not legal, ask for another card
 			if (hand[c].Value() + running_total > 31)
@@ -395,26 +401,35 @@ public:
 
 		return return_card;
 	}
-
+	
+	// ===== SCORE PAIRS =====
+	// will return the score of all pairs in the hand
+	// will not add the score to anything such as 'this->score'
+	// each pair is worth 2 points
 	int score_pairs(deck &p_hand)
 	{
-		int hand_score = 0;
+		int hand_score = 0; // the score of pairs in the hand
 
+		// for each pair of cards in the hand
 		for (int i = 0; i < p_hand.size(); i++)
-		{
 			for (int j = i + 1; j < p_hand.size(); j++)
-			{
+				// if they are a pair, add 2 to the score
 				if (p_hand[i].Rank() == p_hand[j].Rank())
 					hand_score += 2;
-			}
-		}
+		// return the score
 		return hand_score;
 	}
 	
+	// ===== SCORE RUNS =====
+	// will return the score of all runs in 'p_hand'
+	// will not add the score to anything such as 'this->score'
+	// a run is worth 1 point for each card in the run
+	// EX: 3 3 4 5 is worth 6 points from runs as there are 2, 3 card runs
 	int score_runs(deck &p_hand)
 	{
-		int hand_score = 0;
+		int hand_score = 0; // the score of runs in 'p_hand'
 
+		// sort the cards in the hand
 		p_hand.sort();
 
 		for (int i = 0; i < p_hand.size(); i++)
@@ -434,35 +449,49 @@ public:
 				else { break; }
 			}
 
+			// add the last card consequences
 			multiplier *= duplicates;
 			duplicates = 1;
 
 			if (i + dif == p_hand.size() || p_hand[i].Rank() + run_size - 1 != p_hand[i + dif].Rank()) dif--; // if dif is overshooting the run, decrease the dif
 			if (run_size > 2) { hand_score += run_size * multiplier; } // if at least a 3 card run, add the score
-			i += dif;
+			i += dif; // prevent double counting, move i up to the end of the run
 		}
+		// return the score
 		return hand_score;
 	}
 
+	// ===== SCORE 15 =====
+	// will return the score of 15s in 'p_hand'
+	// each unique set of cards that add up to 15 is worth 2 points
+	// the value of the card is equla to the rank unless the rank > 10 where the value is just 10
+	// will not add the score to anything such as 'this->score'
 	int score_15(deck &p_hand)
 	{
-		int hand_score = 0;
+		int hand_score = 0; // the score of the hand
+		
+		// for each set of 2, 3, 4, and 5
 		for (int i = 0; i < p_hand.size(); i++)
 			for (int j = i + 1; j < p_hand.size(); j++)
+				// check sets of 2
 				if (p_hand[i].Value() + p_hand[j].Value() == 15)
 					hand_score += 2;
 				else /*if (p_hand[i].Value() + p_hand[j].Value() < 15)*/
 					for (int k = j + 1; k < p_hand.size(); k++)
+						// check sets of 3
 						if (p_hand[i].Value() + p_hand[j].Value() + p_hand[k].Value() == 15)
 							hand_score += 2;
 						else /*if (p_hand[i].Value() + p_hand[j].Value() + p_hand[k].Value() < 15)*/
 							for (int l = k + 1; l < p_hand.size(); l++)
+								// check sets of 4
 								if (p_hand[i].Value() + p_hand[j].Value() + p_hand[k].Value() + p_hand[l].Value() == 15)
 									hand_score += 2;
 								else /*if (p_hand[i].Value() + p_hand[j].Value() + p_hand[k].Value() + p_hand[l].Value() < 15)*/
 									for (int m = l + 1; m < p_hand.size(); m++)
+										// check the set of 5
 										if (p_hand[i].Value() + p_hand[j].Value() + p_hand[k].Value() + p_hand[l].Value() + p_hand[m].Value() == 15)
 											hand_score += 2;
+		// return the score
 		return hand_score;
 	}
 
